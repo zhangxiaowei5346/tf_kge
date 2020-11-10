acfb8e27a96b788850163eab09a44598d6e5356d
zhangxiaowei5346 <69781227+zhangxiaowei5346@users.noreply.github.com>
2020-11-10 10:35:09 +0800

    Update DB15k_transe.py

--- demo/DB15k_transe.py
+++ demo/DB15k_transe.py
@@ -36,9 +36,6 @@ for epoch in range(10000):
     for step, (batch_h, batch_r, batch_t) in enumerate(
             tf.data.Dataset.from_tensor_slices((train_kg.h, train_kg.r, train_kg.t)).shuffle(10000).batch(
                     train_batch_size)):
-        print(batch_h)
-        print(batch_r)
-        print(batch_t)
         target_entity_type = "head" if np.random.randint(0, 2) == 0 else "tail"
         # for target_entity_type in ["head", "tail"]:
         with tf.GradientTape() as tape:
