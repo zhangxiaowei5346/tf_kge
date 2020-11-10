a1e2b27f7a3d8f0ab719c4b344e70da86e701f74
zhangxiaowei5346 <69781227+zhangxiaowei5346@users.noreply.github.com>
2020-11-10 11:03:59 +0800

    Update DB15k_transe.py

--- demo/DB15k_transe.py
+++ demo/DB15k_transe.py
@@ -67,20 +67,13 @@ for epoch in range(10000):
             print("epoch = {}\tstep = {}\tloss = {}".format(epoch, step, loss))
 
     if epoch % 100 == 0:
-
+        
+        hits_at_ten, hits_at_three, hits_at_one = [], [], []
+        mean_ranks = []
+        mrr = []
         normed_entity_embeddings = tf.math.l2_normalize(model.entity_embeddings, axis=-1)
 
         for target_entity_type in ["head", "tail"]:
-            mean_ranks = []
-            mrr = []
-            
-            hits_at_ten_head, hits_at_ten_head = [], []
-            hits_at_ten_tail, hits_at_ten_tail = [], []
-            hits_at_three_head, hits_at_three_head = [], []
-            hits_at_three_tail, hits_at_three_tail = [], []
-            hits_at_one_head, hits_at_one_head = [], []
-            hits_at_one_tail, hits_at_one_tail = [], []
-            
             for test_step, (batch_h, batch_r, batch_t) in enumerate(
                     tf.data.Dataset.from_tensor_slices((test_kg.h, test_kg.r, test_kg.t)).batch(test_batch_size)):
 
@@ -95,14 +88,12 @@ for epoch in range(10000):
 
                 tiled_entity_embeddings = tf.tile(tf.expand_dims(normed_entity_embeddings, axis=0),
                                                   [batch_h.shape[0], 1, 1])
-                print(tiled_entity_embeddings)
                 tiled_translated = tf.tile(tf.expand_dims(translated, axis=1),
                                            [1, normed_entity_embeddings.shape[0], 1])
                 dis = compute_distance(tiled_translated, tiled_entity_embeddings)
 
                 ranks = tf.argsort(tf.argsort(dis, axis=1), axis=1).numpy()
                 target_ranks = ranks[np.arange(len(batch_target)), batch_target.numpy()]
-                print(target_ranks)
                 
                 for target_rank in target_ranks:
                     if target_rank == 0:
@@ -123,8 +114,12 @@ for epoch in range(10000):
 
                 #print("epoch = {}\ttarget_entity_type = {}\tmean_rank = {}\tmrr = {}".format(epoch, target_entity_type,
                 #                                      np.mean(mean_ranks), np.mean(mrr)))
+                avg_count = np.mean((target_ranks <= 10))
+                hits_at_ten.append(avg_count)
+                avg_count = np.mean((target_ranks <= 3))
+                hits_at_three.append(avg_count)
+                avg_count = np.mean((target_ranks <= 1))
+                hits_at_one.append(avg_count)
 
-                hits = [10, 3, 1]
-                #for hit in hits:
-                #    avg_count = np.mean((target_ranks <= hit))
-                #    print("Hits @ {}: {:.6f}".format(hit, avg_count))
+        print("epoch = {}\tmean_rank = {}\tmrr = {}".format(epoch, np.mean(mean_ranks), np.mean(mrr)))
+        print("Hits @ 10: {:.6f}, Hits @ 3: {:.6f}, Hits @ 1: {:.6f}".format(np.mean(hits_at_ten), np.mean(hits_at_three), np.mean(hits_at_one))
